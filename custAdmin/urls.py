0488f0c319c4aaf93a21d2ea35ba92f4705fc3fd
jakeocinco <jakeocinco@me.com>
2021-08-12 12:02:49 -0400

    Implement data write to csv for golf edit and create with hardcoded datekey

--- custAdmin/urls.py
+++ custAdmin/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name=''),
     path('golf', views.golf_view, name=''),
+    path('edit-golf-<key>', views.edit_golf_classic_request, name=''),
     path('new-golf', views.new_golf_classic_request, name='')
 ]
--- custAdmin/views.py
+++ custAdmin/views.py
@@ -14,7 +14,7 @@ def home(request):
 def golf_view(request):
 
     # context = {'form':form}
-    context = {'golf_outing':process_data_by_event_date_code()}
+    context = {'golf_outing':get_event_data()}
 
     return render(request, 'custAdmin/golf.html', context)
 
@@ -26,15 +26,36 @@ def new_golf_classic_request(request):
         form_results.pop('csrfmiddlewaretoken')
     
 
-        print(form_results)
+        proccess_golf_data(form_results)
 
 
     # form = GolfForm()
     context = {}
     return render(request, 'custAdmin/form.html', context)
 
+def edit_golf_classic_request(request, key):
 
-def process_data_by_event_date_code():
+    if request.method == 'POST':
+        
+        form_results = dict(request.POST)
+        form_results.pop('csrfmiddlewaretoken')
+    
+
+        proccess_golf_data(form_results)
+
+
+    outing_data = get_data_by_event_date_code(key)
+    outing_data['descr'] = '\r\n'.join(outing_data['descr'])
+    print(outing_data)
+    context = {'data':outing_data}
+    return render(request, 'custAdmin/form.html', context)
+
+
+
+
+
+
+def get_event_data():
     golf_main_context = []
     url_main = staticfiles_storage.path('golf_data/golf.csv')
     url_event_schedule = staticfiles_storage.path('golf_data/event_schedule.csv')
@@ -46,3 +67,82 @@ def process_data_by_event_date_code():
             golf_main_context += [d_row]
     return golf_main_context
     
+
+def get_data_by_event_date_code(date_code):
+    
+    golf_main_context = {}
+    url_main = staticfiles_storage.path('golf_data/golf.csv')
+    url_event_schedule = staticfiles_storage.path('golf_data/event_schedule.csv')
+
+    with open(url_main, newline='') as csvfile:
+        spamreader = csv.DictReader(csvfile, delimiter='|', quotechar='|')
+        for row in spamreader:
+            d_row = dict(row)
+            if date_code in d_row['year_key']:
+               golf_main_context = d_row
+    
+    events = []
+    with open(url_event_schedule, newline='') as csvfile:
+        spamreader = csv.DictReader(csvfile, delimiter='|', quotechar='|')
+        for row in spamreader:
+            d_row = dict(row)
+            if date_code in d_row['key']:
+               events += [d_row]
+
+    schedule = {}
+    for e in events:
+        key = e['full_date'] + '_' + e['location']
+        if key not in schedule.keys():
+            schedule.update({key:{
+                'date':e['full_date'],
+                'location':e['location'],
+                'events':[]
+            }})
+
+        schedule[key]['events'] += [{
+            'time':e['time'],
+            'description':e['description'],
+        }]
+
+    schedule = [schedule[x] for x in schedule.keys()]
+
+    return {
+        'year':date_code[0:4],
+        'course':golf_main_context['golf_course'],
+        'date_str':golf_main_context['full_date'],
+        'descr': golf_main_context['description'].split('%&'),
+        'schedule':schedule
+        }
+
+def proccess_golf_data(golf_dict):
+
+    url_main = staticfiles_storage.path('golf_data/golf.csv')
+    content = []
+    with open(url_main, newline='') as csvfile:
+        golf_reader = csv.DictReader(csvfile, delimiter='|', quotechar='|')
+        for row in golf_reader:
+            d_row = dict(row)
+            content += [d_row]
+    
+
+    print(content)
+
+    content = {x['year_key']:x for x in content}
+
+    
+    content.update({'20180512':{
+        'year_key':'20180512', 
+        'full_date':golf_dict['full_date'][0],
+        'golf_course':golf_dict['golf_course'][0],
+        'description':golf_dict['description'][0].replace('\r\n','%&')
+        }})
+
+    print(content)
+
+    with open(url_main, 'w', newline='') as csvfile:
+        fieldnames = ['year_key', 'full_date', 'golf_course', 'description']
+
+        writer = csv.DictWriter(csvfile, delimiter='|', fieldnames=fieldnames)
+        writer.writeheader()
+        
+        writer.writerows([content[x] for x in content.keys()])

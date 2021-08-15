a4840af5d1b1fc98a1cc2296a67c6aa5c3e5fc81
jakeocinco <jakeocinco@me.com>
2021-08-14 23:38:23 -0400

    Next

--- git_publishing/git_publish_all.py
+++ git_publishing/git_publish_all.py
@@ -4,13 +4,17 @@ import os
 def git_publish_all():
 
     if os.getenv('DJANGO_ENV','') != 'local':
-
-        repo = git.Repo(os.getcwd() + '/../')
-        files = repo.git.diff(None, name_only=True)
-        for f in files.split('\n'):
         
-            repo.git.add(f)
+        os.system('git init')
 
+        repo = git.Repo(os.getcwd(), search_parent_directories=True)
+        # files = repo.git.diff(None, name_only=True)
+        # for f in files.split('\n'):
+        
+        #     repo.git.add(f)
+        # print()
+        repo.git.add('.')
         repo.git.commit('-m', 'Deploy new static files')
-        repo.git.push()
+        repo.remote.add('origin','https://github.com/S3Cincinnati/s3cwebsite')
+        repo.git.push('origin','main')
         repo.git.push('heroku','main')

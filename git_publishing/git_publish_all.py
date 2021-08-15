af03948684d68aaed1347d5cc7234d64ad2476f4
jakeocinco <jakeocinco@me.com>
2021-08-15 00:01:07 -0400

    mend

--- git_publishing/git_publish_all.py
+++ git_publishing/git_publish_all.py
@@ -6,15 +6,25 @@ def git_publish_all():
     if os.getenv('DJANGO_ENV','') != 'local':
         
         os.system('git init')
+        os.system("git config --global user.name \"S3Cincy\"")
+        os.system("git config --global user.email \"s3cincinnati.dev@gmail.com\"")
 
-        repo = git.Repo(os.getcwd(), search_parent_directories=True)
+        # repo = git.Repo(os.getcwd(), search_parent_directories=True)
         # files = repo.git.diff(None, name_only=True)
         # for f in files.split('\n'):
         
         #     repo.git.add(f)
         # print()
-        repo.git.add('.')
-        repo.git.commit('-m', 'Deploy new static files')
-        repo.remote.add('origin','https://github.com/S3Cincinnati/s3cwebsite')
-        repo.git.push('origin','main')
-        repo.git.push('heroku','main')
+
+        os.system("git add .")
+        os.system("git commit -m \"Deploy new static files\"")
+        os.system("git remote set-url origin https://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:x-oauth-basic@github.com/S3Cincinnati/s3cwebsite.git")
+        os.system("git push origin main")
+
+        # repo.git.add('.')
+        # repo.git.commit('-m', 'Deploy new static files')
+        # repo.remote.add('origin','https://github.com/S3Cincinnati/s3cwebsite')
+        # repo.git.push('heroku','main')
+        # origin = repo.create_remote('origin1', 'https://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:x-oauth-basic@github.com/S3Cincinnati/s3cwebsite.git')
+        # repo.git.push("--set-upstream", origin, repo.head.ref)
+        # repo.git.push('origin1','main')
